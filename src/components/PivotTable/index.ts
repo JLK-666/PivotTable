3d61d2f8bdcddc879db8ec73dce6b970f6cb6d86
tfs_jlk <1549577516@qq.com>
2024-09-20 11:02:28 +0800

    更新文档

--- src/components/PivotTable/index.ts
+++
@@ -1,7 +0,0 @@
-import type { App } from "vue";
-import PivotTable from "./pivotTable.vue";
-export default {
-  install(app: App<Element>) {
-    app.component(PivotTable.name!, PivotTable);
-  }
-};
